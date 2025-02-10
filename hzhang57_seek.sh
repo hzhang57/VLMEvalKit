7f84e37be56e3624fb94316a905ff085eeefd42d
hzhang57 <zhanghaoinf@outlook.com>
2025-02-10 11:59:22 +0800

    update cpm, seek eval code

--- hzhang57_seek.sh
+++ hzhang57_seek.sh
@@ -2,26 +2,24 @@
 export CUDA_VISIBLE_DEVICES=7
 
 ### DeepSeek-Vl2-Tiny
-#torchrun --nproc-per-node=1 --master_port 29700 run.py --data RealWorldQA  --model deepseek_vl2_tiny --verbose
-#torchrun --nproc-per-node=1 --master_port 29700 run.py --data MMStar  --model deepseek_vl2_tiny --verbose
-#torchrun --nproc-per-node=1 --master_port 29700 run.py --data AI2D_TEST  --model deepseek_vl2_tiny --verbose
-#torchrun --nproc-per-node=1 --master_port 29700 run.py --data SEEDBench_IMG  --model deepseek_vl2_tiny --verbose
+torchrun --nproc-per-node=1 --master_port 29700 run.py --data RealWorldQA  --model deepseek_vl2_tiny --verbose
+torchrun --nproc-per-node=1 --master_port 29700 run.py --data MMStar  --model deepseek_vl2_tiny --verbose
+torchrun --nproc-per-node=1 --master_port 29700 run.py --data AI2D_TEST  --model deepseek_vl2_tiny --verbose
+torchrun --nproc-per-node=1 --master_port 29700 run.py --data SEEDBench_IMG  --model deepseek_vl2_tiny --verbose
+torchrun --nproc-per-node=1 --master_port 29990 run.py --data Video-MME_8frame  --model deepseek_vl2_tiny --verbose
 
 ### DeepSeek-Vl2-Tiny
 torchrun --nproc-per-node=1 --master_port 29700 run.py --data RealWorldQA  --model deepseek_vl2_small --verbose
 torchrun --nproc-per-node=1 --master_port 29700 run.py --data MMStar  --model deepseek_vl2_small --verbose
 torchrun --nproc-per-node=1 --master_port 29700 run.py --data AI2D_TEST  --model deepseek_vl2_small --verbose
 torchrun --nproc-per-node=1 --master_port 29700 run.py --data SEEDBench_IMG  --model deepseek_vl2_small --verbose
+torchrun --nproc-per-node=1 --master_port 29990 run.py --data Video-MME_8frame  --model deepseek_vl2_small --verbose
 
 ### DeepSeek-Vl2
 torchrun --nproc-per-node=1 --master_port 29700 run.py --data RealWorldQA  --model deepseek_vl2 --verbose
 torchrun --nproc-per-node=1 --master_port 29700 run.py --data MMStar  --model deepseek_vl2 --verbose
 torchrun --nproc-per-node=1 --master_port 29700 run.py --data AI2D_TEST  --model deepseek_vl2 --verbose
 torchrun --nproc-per-node=1 --master_port 29700 run.py --data SEEDBench_IMG  --model deepseek_vl2 --verbose
+torchrun --nproc-per-node=1 --master_port 29990 run.py --data Video-MME_8frame  --model deepseek_vl2_small --verbose
 
-### MiniCPM-v2.6
-#torchrun --nproc-per-node=1 --master_port 29700 run.py --data RealWorldQA  --model MiniCPM-V-2_6 --verbose
-#torchrun --nproc-per-node=1 --master_port 29700 run.py --data MMStar  --model MiniCPM-V-2_6 --verbose
-#torchrun --nproc-per-node=1 --master_port 29700 run.py --data AI2D_TEST  --model MiniCPM-V-2_6 --verbose
-#torchrun --nproc-per-node=1 --master_port 29700 run.py --data SEEDBench_IMG  --model MiniCPM-V-2_6 --verbose
 
