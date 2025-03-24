2653ae90de2f076cebe1954008cf5eec4fac2c01
zhangh4 <zhangh4@ihpc.a-star.edu.sg>
2025-03-24 11:50:34 +0800

    add random

--- hzhang57_random.sh
+++ hzhang57_random.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 
 ### ViLA1.5-3b
 #torchrun --nproc-per-node=1 --master_port 28735 run.py --data RealWorldQA  --model VILA1.5-3b --verbose
